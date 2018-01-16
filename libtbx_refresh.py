1bd2e0cf6bd4a47ac04c73d450c4660bad391038
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-01-16 09:16:24 +0000

    compatibility for libtbx changes

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -1,36 +1,30 @@
+from __future__ import absolute_import, division, print_function
 import os
 from annlib_adaptbx.source_generators import generate_all
 
-if (self.env.is_ready_for_build()):
-  message = '  Generating C++ files for self-inclusive approximate nearest neighbor'
-  print message
+if self.env.is_ready_for_build():
+  print('  Generating C++ files for self-inclusive approximate nearest neighbour')
   for search_mode in ["self_include"]:
     for item in generate_all.yield_includes(search_mode):
       #print item
-      G = open(item['src'],'r')
-      lines = G.readlines()
-      G.close()
+      with open(item['src'],'r') as G:
+        lines = G.readlines()
       generate_all.process_includes(lines,search_mode)
-      F = open(item['dst'],'w')
-      F.write(''.join(lines))
-      F.close()
+      with open(item['dst'],'w') as F:
+        F.write(''.join(lines))
 
     for item in generate_all.yield_src_includes(search_mode):
       #print item
-      G = open(item['src'],'r')
-      lines = G.readlines()
-      G.close()
+      with open(item['src'],'r') as G:
+        lines = G.readlines()
       generate_all.process_src_includes(lines,search_mode)
-      F = open(item['dst'],'w')
-      F.write(''.join(lines))
-      F.close()
+      with open(item['dst'],'w') as F:
+        F.write(''.join(lines))
 
     for item in generate_all.yield_src(search_mode):
       #print item
-      G = open(item['src'],'r')
-      lines = G.readlines()
-      G.close()
+      with open(item['src'],'r') as G:
+        lines = G.readlines()
       generate_all.process_src(lines,search_mode,os.path.basename(item['src']))
-      F = open(item['dst'],'w')
-      F.write(''.join(lines))
-      F.close()
+      with open(item['dst'],'w') as F:
+        F.write(''.join(lines))
