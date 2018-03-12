d5c3457bd4f1c7b1cf385034737466587244574d
Nicholas Devenish <ndevenish@gmail.com>
2018-03-12 18:18:42 +0000

    Change to standard libtbx-esque runtests

--- run_tests.py
+++ run_tests.py
@@ -1,16 +1,8 @@
-from libtbx import test_utils
-import libtbx.load_env
+from __future__ import absolute_import, division, print_function
 
-def run():
-  tst_list = (
+from libtbx.test_utils.pytest import discover
+
+tst_list = [
   "$D/annlib_adaptbx/sample.py",
   "$D/annlib_adaptbx/self_match.py",
-  )
-
-  build_dir = libtbx.env.under_build("annlib_adaptbx")
-  dist_dir = libtbx.env.dist_path("annlib_adaptbx")
-
-  test_utils.run_tests(build_dir, dist_dir, tst_list)
-
-if (__name__ == "__main__"):
-  run()
+] + discover()
